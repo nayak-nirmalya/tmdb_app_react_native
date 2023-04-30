da5b971d4bb63510a8bd442f5bd9e47611ab0e15
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-30 14:12:28 +0530

    added React Native for Windows

---
+++ windows/MovieDBProject/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::MovieDBProject::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::MovieDBProject::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
