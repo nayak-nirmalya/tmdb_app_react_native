838ee4b7f893a1eb80a4a42ffaab54d387833bfa
Nirmalya Nayak <n.nayak263@gmail.com>
2023-05-01 14:06:39 +0530

    moved type declaration to types.ts fiel

--- App.tsx
+++ App.tsx
@@ -1,33 +1,21 @@
 import axios from 'axios';
-import React, { useState } from 'react';
 import type { PropsWithChildren } from 'react';
+import React, { useEffect, useState } from 'react';
 import { SafeAreaView, StyleSheet, Text, useColorScheme } from 'react-native';
 
 import { TMDB_API_KEY } from '@env';
+import { Movie } from './types';
 
 type SectionProps = PropsWithChildren<{
   title: string;
 }>;
 
-type Movie = {
-  adult: boolean;
-  backdrop_path: string;
-  genre_ids: number[];
-  id: number;
-  original_language: string;
-  original_title: string;
-  overview: string;
-  popularity: number;
-  poster_path: string;
-  release_date: string;
-  title: string;
-  video: boolean;
-  vote_average: number;
-  vote_count: number;
+export type MoviesResponse = {
+  results: Movie[];
 };
 
-const getPopularMovies = async () => {
-  const response = await axios.get(
+const getPopularMovies = async (): Promise<Movie[]> => {
+  const response = await axios.get<MoviesResponse>(
     `https://api.themoviedb.org/3/movie/popular?api_key=${TMDB_API_KEY}`
   );
   return response.data.results;
@@ -37,16 +25,38 @@ function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
   const [movie, setMovie] = useState<Movie | null>(null);
+  const [error, setError] = useState(false);
 
-  const data = getPopularMovies().then((movies) => {
-    setMovie(movies[0]);
-  });
+  useEffect(() => {
+    getPopularMovies()
+      .then((movies) => {
+        setMovie(movies[0]);
+      })
+      .catch((err) => {
+        console.error(err);
+        setError(!!err);
+      });
+  }, []);
 
   return (
     <SafeAreaView className="h-full bg-white">
       <Text className="text-black font-bold text-lg items-center mx-auto justify-center">
         {movie?.original_title}
       </Text>
+      <Text className="text-black font-bold text-lg items-center mx-auto justify-center">
+        {movie?.overview}
+      </Text>
+      <Text className="text-black font-bold text-lg items-center mx-auto justify-center">
+        {movie?.release_date}
+      </Text>
+      <Text className="text-black font-bold text-lg items-center mx-auto justify-center">
+        {movie?.vote_average}
+      </Text>
+      {error && (
+        <Text className="text-red-500 font-bold text-lg items-center mx-auto justify-center">
+          Error
+        </Text>
+      )}
     </SafeAreaView>
   );
 }
