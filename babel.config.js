57047d0a903113b16cb8ebd58910033d7c224135
Nirmalya Nayak <n.nayak263@gmail.com>
2023-05-01 10:25:36 +0530

    configured dot-env plugin

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,16 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ["nativewind/babel"],
+  plugins: [
+    'nativewind/babel',
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        path: '.env',
+        whitelist: ['TMDB_API_KEY', 'TMDB_READ_ACCESS_TOKEN'],
+        safe: false,
+        allowUndefined: true,
+      },
+    ],
+  ],
 };
