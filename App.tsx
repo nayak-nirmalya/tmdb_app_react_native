6893b5ef16505674a839d962ce27abd3eb703c5b
Nirmalya Nayak <n.nayak263@gmail.com>
2023-05-02 13:44:55 +0530

    configured nativ-stack

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import type { PropsWithChildren } from 'react';
 import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Home from './screens/Home';
 
@@ -9,10 +10,25 @@ type SectionProps = PropsWithChildren<{
   title: string;
 }>;
 
+export type RootStackParamList = {
+  Home: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
-  return <Home />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{ headerShown: false }}
+        initialRouteName="Home"
+      >
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({
