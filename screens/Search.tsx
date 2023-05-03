c900caeafce3d1706c49d6117c8b8d17296a275c
Nirmalya Nayak <n.nayak263@gmail.com>
2023-05-03 11:41:49 +0530

    added text input field to search screen

--- screens/Search.tsx
+++ screens/Search.tsx
@@ -1,10 +1,10 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import React, { useState } from 'react';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { RootStackParamList } from '../App';
 import { Movie, TV } from '../types';
-import { SafeAreaView } from 'react-native-safe-area-context';
 
 export type SearchScreenProps = {
   searchResult?: Movie | TV;
@@ -13,11 +13,19 @@ export type SearchScreenProps = {
 type SearchProps = NativeStackScreenProps<RootStackParamList, 'Search'>;
 
 const Search = ({ route, navigation }: SearchProps): JSX.Element => {
+  const [search, setSearch] = useState('');
+
   return (
     <>
       <SafeAreaView>
         <View>
-          <Text>SearchScreen</Text>
+          <TextInput
+            className="mt-52"
+            value={search}
+            onChangeText={setSearch}
+            style={styles.input}
+            placeholder="Search Movie / TV"
+          />
         </View>
       </SafeAreaView>
     </>
@@ -26,4 +34,10 @@ const Search = ({ route, navigation }: SearchProps): JSX.Element => {
 
 export default Search;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+  },
+});
